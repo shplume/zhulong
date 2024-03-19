ed42eb23e8c84228bba144404d8ecb29b3e9ed86
Jundan Zhang <1750132640@qq.com>
2024-03-19 22:40:24 +0800

    Feature: ent ORM and mysql client

---
+++ ent/schema/user.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("id").Unique(),
+		field.Int("role"),
+		field.String("account").Unique(),
+		field.String("password"),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return nil
+}
